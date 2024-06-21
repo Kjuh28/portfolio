5dbc7f9a1f4badef14abfc6b62796abdebc2c50a
Kjuh28 <kjuh28@gmail.com>
2024-06-21 17:51:00 -0300

    Realizado alguns testes de posição da div na page projetos

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,10 +1,11 @@
 export function Project({id, name, description}){
-    {(id % 2 == 0 ? console.log(`${id} é par`) : console.log(`${id} é impar`) )}
     return(
-        <div id={id} className="text-3xl text-center p-8 m-8 border border-dashed rounded-xl ">
-            <h1 className="text-orange_400 font-bold text-6xl">{name}</h1>
-            <p>{description}</p>
+        <div id={id} className='text-3xl grid justify-items-scretch text-center p-8 m-8 border border-dashed rounded-xl'>
+            <div className={(id % 2 == 0 ? 'justify-self-start' : 'justify-self-end')}>
+                <h1 className="font-bold text-6xl">{name}</h1>
+                <p>{description}</p>
+            </div>
+            
         </div>
-        
     )
 }
