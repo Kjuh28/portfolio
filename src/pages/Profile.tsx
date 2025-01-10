9d007ff024692107252bbccc78d51ba6feb6bab3
Kjuh28 <kjuh28@gmail.com>
2025-01-10 10:20:55 -0300

    refactor: (link) change link of cv and add target blank

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,8 +5,8 @@ export function Profile(){
         <section id="profile" className="mx-auto p-8 sm:mx-32">
             <div className="place-items-center sm:my-8 grid lg:flex xl:justify-between text-center lg:text-start">
                 <div className="max-w-xl w-full ">
-                    <p className="sm:text-4xl text-3xl flex flex-col gap-4">Olá, eu sou o <a className="font-bold text-3xl sm:text-6xl text-orange_400 sm:hover:text-orange_600" href='https://github.com/Kjuh28'>Kerley Sousa</a> Desenvolvedor Front-end  </p>
-                    <footer className="pt-8"><a href="https://drive.google.com/file/d/1rKOIn8wPZeHAkwh-ZJqt314Cj30jtQpa/view?usp=sharing" download target="_blank"><Button tittle='Download CV' /> </a></footer>
+                    <p className="sm:text-4xl text-3xl flex flex-col gap-4">Olá, eu sou o <a className="font-bold text-3xl sm:text-6xl text-orange_400 sm:hover:text-orange_600" href='https://github.com/Kjuh28' target="_blank" rel="noopener">Kerley Sousa</a> Desenvolvedor Full Stack  </p>
+                    <footer className="pt-8"><a href="https://drive.google.com/file/d/1MRJ2IRjBi5YT4nQKQ1doUxPTPUu0VrcK/view?usp=drive_link" download target="_blank" rel="noopener"><Button tittle='Download CV' /> </a></footer>
                 </div>
                 <div className="max-w-xl w-full flex justify-center">
                     <img src="https://raw.githubusercontent.com/Kjuh28/portfolio/main/src/assets/avatar500.png" className="" ></img>
