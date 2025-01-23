6d2e0e71e50356ce72122fd76476f9985098341f
Kjuh28 <kjuh28@gmail.com>
2025-01-23 17:44:43 -0300

    feat:(handleOpenComponents) create handleOpenComponents function

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -1,33 +1,50 @@
 import { useState, useRef } from "react"
 import { useOnClickOutside } from 'usehooks-ts'
-export interface abilityProps {
+import { Modal } from "./Modal"
+export interface AbilityProps {
     title: string,
-    tagIcon: string,
-    description: string
+    icon_link: string,
+    description: string,
+    icon_type: number,
 }
 
-export function Collapse({title, tagIcon, description}:abilityProps){
-    const [openAccordion, setOpenAccordion] = useState(false)
+export function Collapse({title, icon_link, description, icon_type}:AbilityProps){
+    const [openComponents, setOpenComponents] = useState(false)
     const openRef = useRef(null)
 
     const handleClickOutside = () => {
-        setOpenAccordion(false)
+        setOpenComponents(false)
     }
-    
+    const handleOpenComponents = () =>{
+            setOpenComponents(!openComponents)
+    }
+
     useOnClickOutside(openRef, handleClickOutside)
 
     return(
         <>
-            <div className='text-xl sm:text-2xl p-14 relative' >
+            <div tabIndex={icon_type} className='text-xl sm:text-2xl p-6 sm:p-14 relative sm:min-w-80' >
                     <div ref={openRef}>
-                        <button  className='justify-self-center hover:scale-110 transition delay-100 p-2 max-w-24 sm:max-w-[96px]' onClick={() => setOpenAccordion(!openAccordion)}><img src={tagIcon}></img></button>
+                        <button  className='justify-self-center hover:scale-110 transition delay-100 p-2 max-w-20 sm:max-w-[96px]' onClick={() => setOpenComponents(!openComponents)}><img src={icon_link}></img></button>
                         <div className={`text-white text-center transition-all duration-300 ease-in-out`} >
-                            {openAccordion && <div className={`p-4 absolute border border-dashed rounded-xl border-orange_400 left-0 bg-black z-10`}>
-                                <h2 className="text-orange_400 ">{title}</h2>
-                                <p className="text-justify">{description}</p>           
-                        </div> }
-                    </div>                    
-                </div>
+                            { openComponents && 
+                                <div>
+                                    <div className={`hidden sm:inline-block sm:left-0 p-4 absolute border border-dashed rounded-xl border-orange_400 bg-black z-10`}>
+                                        <h2 className="text-orange_400">{title}</h2>
+                                        <p className="text-left">{description}</p>           
+                                    </div> 
+                                    <Modal
+                                        title={title}
+                                        description={ description}
+                                        icon_link={icon_link}
+                                        icon_type={icon_type}
+                                        handleOpenComponents={handleOpenComponents}
+                                        openComponents= {openComponents}
+                                    />
+                                </div> 
+                            }
+                        </div>                    
+                    </div>
             </div>
         </>
     )
