0b15c7125fa02195c790c06c76fb1afd26bcfb9d
Kjuh28 <kjuh28@gmail.com>
2025-11-14 13:30:26 -0300

    fix(project): update link behavior to open modal instead of navigating to page

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -23,15 +23,15 @@ export function Project({id, title, description, image, git_link, page_link, ind
         <div id={id} className='p-8 mb-8 border border-dashed border-orange_200 rounded-xl text-center place-items-center'>
             <div className={(Number(index) % 2 == 0 ? 'sm:flex sm:justify-between xl:max-h-96 gap-8' : 'sm:flex sm:flex-row-reverse xl:justify-between  gap-8')}>
                 <div className="sm:max-w-xl w-full">
-                    <a href={page_link} target="_blank" rel="noopener"><img className="w-full h-full object-cover object-center rounded-xl hover:scale-110 transition-transform" src={image}/></a>
+                    <a onClick={handleOpenModalCard} rel="noopener"><img className="w-full h-full object-cover object-center rounded-xl hover:scale-110 transition-transform" src={image}/></a>
                 </div>
                 <div className="sm:w-full max-w-xl">
                     <h1 className="font-bold text-3xl sm:text-4xl w-full text-orange_400 pb-8 pt-8">{title}</h1>
                     <p className="text-xl sm:text-2xl line-clamp-3 sm:line-clamp-none">{description}</p>
                     <footer className="pt-8 sm:flex sm:justify-around">
                         <a className="grid p-4 sm:w-44" href={git_link} target="_blank" rel="noopener">{<Button tittle="Repositório" />}</a>
-                        <a className="grid p-4 sm:w-44"  rel="noopener" onClick={handleOpenModalCard}>
-                            <Button tittle='Ver mais'/>
+                        <a className="grid p-4 sm:w-44"  rel="noopener">
+                            <a className='grid' onClick={handleOpenModalCard}><Button tittle='Ver mais'/></a>
                             {
                             <ModalCard
                                 id={id}
