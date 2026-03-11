aeca6fb68b49dac68fe3239c8416ee388ae25b3f
Kjuh28 <kjuh28@gmail.com>
2026-03-11 18:02:20 -0300

    feat(project): adjust image height in Project component for better responsiveness

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -26,7 +26,7 @@ export function Project({id, title, description, image, git_link, page_link, ind
         <div id={id} className='p-8 mb-8 border border-dashed border-orange_200 rounded-xl text-center place-items-center'>
             <div className={(Number(index) % 2 == 0 ? 'sm:flex sm:justify-between xl:max-h-96 gap-8' : 'sm:flex sm:flex-row-reverse xl:justify-between  gap-8')}>
                 <div className="sm:max-w-xl w-full">
-                    <a onClick={handleOpenModalCard} rel="noopener"><img className="w-full h-full object-cover object-center rounded-xl hover:scale-110 transition-transform" src={image}/></a>
+                    <a onClick={handleOpenModalCard} rel="noopener"><img className="w-full sm:h-full h-32 object-cover object-center rounded-xl hover:scale-110 transition-transform" src={image}/></a>
                 </div>
                 <div className="sm:w-full max-w-xl">
                     <h1 className="font-bold text-3xl sm:text-4xl w-full text-orange_400 pb-8 pt-8">{title}</h1>
